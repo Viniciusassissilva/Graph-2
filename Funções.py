12eb0e5a09221d2ab875ff46ee258d2524bf8088
Vinicius Assis <49870438+Viniciusassissilva@users.noreply.github.com>
2021-08-24 22:10:10 -0300

    Add files via upload

--- Funções.py
+++ Funções.py
@@ -63,7 +63,6 @@ def contrutivo(Lista):
     vvisitado[0] = 1
     while len(naovisitados) != 1:
         menor = float('inf')
-
         for i in Lista[u]:
             if i[1] < menor and vvisitado[i[0]] == 0:
                 menor = i[1]
@@ -80,18 +79,23 @@ def refinamento(tempo, percuso, Matriz):
     #2-OPT
     aux1 = []
     aux2 = 0
-
+    tentativa = []
     for _ in tqdm(range(tempo), desc="Refinando...", ascii=False, ncols=75):
         vertice = randint(1, len(percuso) - 2)
         vertice2 = randint(1, len(percuso) - 2)
 
-        if vertice != vertice2 :
+        if vertice != vertice2 and (vertice, vertice2) not in tentativa:
+
+            tentativa.append((vertice, vertice2))
+
             aux1 = copy.deepcopy(percuso)
             aux2 = aux1[vertice]
             aux1[vertice] = aux1[vertice2]
             aux1[vertice2] = aux2
             if tamnhanho_percuso(aux1, Matriz) < tamnhanho_percuso(percuso, Matriz):
                 percuso = copy.deepcopy(aux1)
+                print(tamnhanho_percuso(aux1, Matriz))
+                tentativa = []
 
         time.sleep(0.99)
 
